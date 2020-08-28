13a98016de0c8309a36d13d98f7218af136aba09
黄兵 <1278032416@qq.com>
2020-08-28 09:46:21 +0800

    update

--- src/views/system/index/components/d2-page-cover/index.vue
+++ src/views/system/index/components/d2-page-cover/index.vue
@@ -6,13 +6,6 @@
     <p class="d2-page-cover__title">D2 Admin {{$version}}</p>
     <p class="d2-page-cover__sub-title">优雅的中后台集成方案</p>
     <p class="d2-page-cover__build-time">FINAL BUILD TIME {{$buildTime}}</p>
-    <slot name="footer"/>
-    <a target="blank" href="https://github.com/d2-projects/d2-admin">
-      <img
-        style="position: absolute; top: 0; right: 0; border: 0; width: 150px;"
-        src="./image/darkblue@2x.png"
-        alt="Fork me on GitHub">
-    </a>
   </div>
 </template>
 
